365d5393ef4ddd8e72e3b39873770edce27209e9
aboveStars <yunuskorkmazis384@outlook.com>
2024-10-10 21:52:49 +0300

    Error handling.

--- main.cpp
+++ main.cpp
@@ -13,11 +13,39 @@ int main(){
     int currentSize = 0;
 
     while(true) {
-        cout << "Please input your choice: " << endl;
 
-        int value = 0;
-        cin >> value;
-        if(value == -1) break;
+        cout << "Please enter an age for " << currentSize + 1 << ". student (-1 to exit): " << endl;
+
+        int age = 0;
+
+        while (true) {
+            cin >> age;
+
+            if(cin.fail()) {
+                cin.clear();
+                cin.ignore();
+                cout << "Omgggg! Please enter an age (int) for " << currentSize + 1 << ". student (-1 to exit): " << endl;
+            } else {
+                break;
+            }
+        }
+
+        if(age == -1) break;
+
+        double grade = 0;
+        cout << "Please enter a grade for " << currentSize + 1 << ". student" << endl;
+
+        while(true) {
+            cin >> grade;
+
+            if(cin.fail()) {
+                cin.clear();
+                cin.ignore();
+                cout << "Broooo! Please enter a grade for " << currentSize + 1 << ". student" << endl;
+            } else {
+                break;
+            }
+        }
 
         Student* temp = (Student*)realloc(students, sizeof(Student) * (currentSize + 1));
 
@@ -25,10 +53,11 @@ int main(){
             cout << "Memory allocation error" << endl;
             return -1;
         }
+
         students = temp;
 
-        students[currentSize].age = value;
-        students[currentSize].grade = value * 0.5;
+        students[currentSize].age = age;
+        students[currentSize].grade = grade;
 
         currentSize++;
     }
