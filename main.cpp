0f976b041967d3252aeb4f7d175a642aedf140be
aboveStars <yunuskorkmazis384@outlook.com>
2024-10-10 21:30:27 +0300

    Students

--- main.cpp
+++ main.cpp
@@ -2,9 +2,14 @@
 
 using namespace std;
 
+typedef struct{
+    int age;
+    double grade;
+} Student;
+
 int main(){
 
-    int* asks = nullptr;
+    Student* students = nullptr;
     int currentSize = 0;
 
     while(true) {
@@ -14,24 +19,25 @@ int main(){
         cin >> value;
         if(value == -1) break;
 
-        int* temp = (int*)realloc(asks, sizeof(int) * (currentSize + 1));
+        Student* temp = (Student*)realloc(students, sizeof(Student) * (currentSize + 1));
 
         if(temp == nullptr) {
             cout << "Memory allocation error" << endl;
             return -1;
         }
-        asks = temp;
+        students = temp;
 
-        asks[currentSize] = value;
+        students[currentSize].age = value;
+        students[currentSize].grade = value * 0.5;
 
         currentSize++;
     }
 
-    for(int i=0; i< currentSize && asks != nullptr; i++) {
-        cout <<asks[i] << endl;
+    for(int i=0; i< currentSize && students != nullptr; i++) {
+        cout << "Age: " << students[i].age << " Grade: " << students[i].grade << endl;
     }
 
-    free(asks);
+    free(students);
 
     return 0;
 }
