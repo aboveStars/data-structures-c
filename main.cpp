2a64ea662c13f241b24c01a90d8e4651e5ef9123
aboveStars <yunuskorkmazis384@outlook.com>
2024-10-10 20:54:02 +0300

    Hello World!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 
+using namespace std;
+
 int main(){
-    std::cout << "Hello, World!" << std::endl;
+
+    int* asks = nullptr;
+    int currentSize = 0;
+
+    while(true) {
+        cout << "Please input your choice: " << endl;
+
+        int value = 0;
+        cin >> value;
+        if(value == -1) break;
+
+        int* temp = (int*)realloc(asks, sizeof(int) * (currentSize + 1));
+
+        if(temp == nullptr) {
+            cout << "Memory allocation error" << endl;
+            return -1;
+        }
+        asks = temp;
+
+        asks[currentSize] = value;
+
+        currentSize++;
+    }
+
+    for(int i=0; i< currentSize && asks != nullptr; i++) {
+        cout <<asks[i] << endl;
+    }
+
+    free(asks);
+
     return 0;
 }
