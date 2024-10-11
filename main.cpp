b45fc77d59648d5c1683c0fdf54e874f272528cd
aboveStars <yunuskorkmazis384@outlook.com>
2024-10-11 16:14:01 +0300

    Finding middle element - naive.

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 
 using namespace std;
 
-typedef struct Node{
+typedef struct Node {
     int data;
     Node* next;
 } Node;
+
 
 Node* createNode(int data) {
     Node* newNode = (Node*) malloc(sizeof(Node));
@@ -16,20 +17,45 @@ Node* createNode(int data) {
     return newNode;
 }
 
+int getLength(Node* head) {
+    Node* temp = head;
+
+    int length = 0;
+    while(temp) {
+        length++;
+        temp = temp->next;
+    }
+
+    return length;
+
+}
+
+int getMiddleIndex(int length) {
+    return length / 2;
+}
+
 int main() {
 
     Node* head = createNode(10);
-    head->next = createNode(20);
-    head->next->next = createNode(30);
+    head -> next = createNode(20);
+    head -> next -> next = createNode(30);
+    head -> next -> next -> next  = createNode(40);
+    head -> next -> next -> next -> next = createNode(50);
+    head -> next -> next -> next -> next -> next= createNode(60);
 
-    cout << "Linked List: " <<endl;
+    int length = getLength(head);
 
-    Node* current = head;
+    int middleIndex = getMiddleIndex(length);
 
-    for(int i=0; i< 3; i++) {
-        cout << current->data<<endl;
-        current = current->next;
+    Node* temp = head;
+    int currentIndex = 0;
+
+    while(currentIndex != middleIndex) {
+        temp = temp -> next;
+        currentIndex++;
     }
+
+    cout << "Middle Element: " << temp->data << endl;
 
     free(head);
 
