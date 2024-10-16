3771522f152b82d8d7ba06b268e5d8512e364734
aboveStars <yunuskorkmazis384@outlook.com>
2024-10-16 16:05:29 +0300

    Find number of elements in circular linked list.

--- main.cpp
+++ main.cpp
@@ -1,27 +1,58 @@
 #include <iostream>
-#include "include/LinkedList.h"
 
 using namespace std;
 
-/**
-Node init_empty_node(int data, Node* next) {
-    Node newNode;
 
-    newNode.data = data;
-    newNode.next = next;
+typedef struct Node {
+    int data;
+    Node* next;
+}Node;
+
+Node* createNode(int data) {
+
+    Node* newNode =  (Node*)malloc(sizeof(Node));
+
+    newNode->data = data;
+    newNode->next = nullptr;
 
     return newNode;
+
 }
-*/
+
+int countLinkedList(Node* head) {
+
+    Node* current = head->next;
+
+    int count = 1;
+
+    while(current != nullptr && current != head) {
+        count++;
+        current = current->next;
+    }
+
+    return count;
+}
 
 
 int main() {
 
-    LinkedList l;
-    add_to_empty(l, 5);
-    add_to_tail(l, 3);
+    Node* head = createNode(1);
 
-    traverse(l);
+    head->next = createNode(2);
+    head->next->next = createNode(3);
+    head->next->next->next = createNode(4);
+    head->next->next->next->next = createNode(5);
+    head->next->next->next->next->next= createNode(6);
+    //head->next->next->next->next->next->next = head;
+
+    int count = countLinkedList(head);
+    cout << "Count: " << count << endl;
+
+
+
+
+    free(head);
+
 
     return 0;
 }
